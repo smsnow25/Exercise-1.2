1ec0c3a13abbd1eab1d014377328dcd9c6f968ca
smsnow25 <139315216+smsnow25@users.noreply.github.com>
2023-12-23 17:58:04 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*  1.2 Write a program that displays Welcome to C++ five times. */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to C++" << endl;
+    cout << "Welcome to C++" << endl;
+    cout << "Welcome to C++" << endl;
+    cout << "Welcome to C++" << endl;
+    cout << "Welcome to C++" << endl;
+    
+    return 0;
+}
